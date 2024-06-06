6aa5114ecc028606aaba905894582d096bc8bce5
Alexander Bazzi <codemaster@xbazzi.com>
2024-06-06 11:29:18 -0600

    Add logic to SquareRoot.cpp

--- newton_raphson/SquareRoot.cpp
+++ newton_raphson/SquareRoot.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-/// @brief Square root computation using the Newton-Raphson method
+/// @brief Square root computation using the Newton-Raphson method.
+///         This method solves the y = x^2 equation for x given y.
 /// @return Square root of number
 auto sqroot(double number) -> const double {
-    
+    double curr_der, curr_f;
+    while (curr_tol > tol) {
+        
+    }
 }
