d00fe402608b5e86153186a49f936f08e2c014ca
Xander Bazzi <xander@xbazzi.com>
2025-02-04 03:31:03 -0500

    Start monty hall simulator

---
+++ stats/MontyHall.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class MontyHall
+{
+private:
+protected:
+
+public:
+    std::vector<Outcomes>
+
+    auto run_sim() const -> const double
+    {
+
+    }
+};
+
+class Outcomes
+{
+
+}
+
+int main()
+{
+
+    return 0;
+}
